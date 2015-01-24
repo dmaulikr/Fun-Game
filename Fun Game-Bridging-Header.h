4d6672c0973b4e1ba7c17f3603995efca6ec5347
Lucas Haber <lhaber@carecloud.com>
2015-01-24 15:47:44 -0500

    First Commit

---
+++ Fun Game-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Fun Game-Bridging-Header.h
+//  Fun Game
+//
+//  Created by Lucas Haber on 1/24/15.
+//  Copyright (c) 2015 lhaber. All rights reserved.
+//
+
+#ifndef Fun_Game_Fun_Game_Bridging_Header_h
+#define Fun_Game_Fun_Game_Bridging_Header_h
+
+#import "RoutingHTTPServer.h"
+
+
+
+#endif
---
+++ Fun Game/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Fun Game
+//
+//  Created by Lucas Haber on 1/24/15.
+//  Copyright (c) 2015 lhaber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var http: RoutingHTTPServer;
+    
+    required init(coder aDecoder: NSCoder) {
+        http = RoutingHTTPServer()
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        http.setPort(8000)
+        http.setDefaultHeader("Server", value: "Fun Game/1.0")
+        
+        
+        
+        http.get("/hello", withBlock: { (request: RouteRequest!, response: RouteResponse!) -> Void in
+            response.setHeader("Content-Type", value: "text/plain")
+            response.respondWithString("Hello!")
+        })
+        
+        
+        http.start(nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
